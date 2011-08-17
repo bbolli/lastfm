75bc64ddc7fe1c0e1b71e197342b31b94423e78e
Beat Bolli <bbolli@ewanet.ch>
2011-08-17 12:27:44 +0200

    Isolate the function that generates the first n ranks from the charts

--- charts.py
+++ charts.py
@@ -7,13 +7,20 @@ an Atom feed with the most played artists."""
 # configuration
 DOMAIN = 'drbeat.li'
 PATH = '/lastfm.atom'
-MAX = 3
+RANKS = 3
 
 import sys, datetime
+from itertools import groupby, islice
+
 import xmltramp, xmlbuilder
 
 ATOM_NS = 'http://www.w3.org/2005/Atom'
 XHTML_NS = 'http://www.w3.org/1999/xhtml'
+
+def first_n_ranks(items, n, keyfunc):
+    for key, group in islice(groupby(items, keyfunc), n):
+        for i in group:
+            yield i
 
 def fetch_weekly_charts(user_id):
     url = 'http://ws.audioscrobbler.com/2.0/user/%s/weeklyartistchart.xml' % user_id
@@ -43,16 +50,13 @@ def make_feed(charts):
             f.link(None, rel='alternate', type='text/html', href=lastfm + '/charts?charttype=weekly')
             f.category(None, term='charts')
             f.category(None, term='music')
-            clast = None; i = 0
             with f.content(type='xhtml').div(xmlns=XHTML_NS).ol:
-                for artist in charts['artist':]:
-                    c = str(artist.playcount).strip()
-                    if c != clast:
-                        clast = c; i += 1
-                        if i > MAX: break
+                for artist in first_n_ranks(charts['artist':], RANKS,
+                    lambda a: str(a.playcount)
+                ):
                     with f.li:
                         f.a(unicode(artist.name), href=str(artist.url))
-                        f['(%s)' % c]
+                        f['(%s)' % artist.playcount]
     return str(f)
 
 def application(environ, start_response):

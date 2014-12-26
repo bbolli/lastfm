c2b018b2bb2b84ce51241d10860a63ac4a3f2319
Beat Bolli <dev@drbeat.li>
2014-12-26 13:38:16 +0100

    Use an object to hold the entry data
    It's always the same: first you think you can get by without objects,
    then you do the first enhancement, then you need them anyway.

--- charts.py
+++ charts.py
@@ -40,38 +40,42 @@ def prune_charts(charts):
             del charts[a]
     return hasattr(charts, 'artist')
 
-def make_feed(charts):
-    who = charts('user')
-    when = datetime.datetime.utcfromtimestamp(int(charts('to')))
-    when = when.isoformat() + 'Z'
-    tags = ('charts', 'music', 'last.fm')
-    artists = [dict(name=unicode(a.name), url=str(a.url), playcount=playcount(a))
-        for a in first_n_ranks(charts['artist':], RANKS, playcount)
-    ]
 
-    f = xmlbuilder.builder()
-    with f.feed(xmlns=ATOM_NS):
-        lastfm = 'http://www.last.fm/user/%s' % who
-        f.title(u"Meine last.fm-Hitparade")
-        with f.author:
-            f.name(who)
-        f.link(None, href=lastfm)
-        f.updated(when)
-        f.id('tag:drbeat.li,2010:lastfmcharts:%s' % who)
-        f.link(None, rel='self', href='http://%s%s' % (DOMAIN, PATH))
-        with f.entry:
-            f.title(u"Meist gespielte Bands vom %s" % when[:10])
-            f.updated(when)
-            f.id('tag:%s,%s:%s:%s' % (DOMAIN, when[:10], PATH, who))
-            f.link(None, rel='alternate', type='text/html', href=lastfm + '/charts?charttype=weekly')
-            for term in tags:
-                f.category(None, term=term)
-            with nested(f.content(type='xhtml'), f.div(xmlns=XHTML_NS), f.ol):
-                for artist in artists:
-                    with f.li:
-                        f.a(artist['name'], href=artist['url'])
-                        f['(%s)' % artist['playcount']]
-    return str(f)
+class Entry:
+    def __init__(self, charts):
+        self.who = charts('user')
+        self.ts = datetime.datetime.utcfromtimestamp(int(charts('to')))
+        self.when = self.ts.isoformat() + 'Z'
+        self.tags = ('charts', 'music', 'last.fm')
+        self.artists = [dict(name=unicode(a.name), url=str(a.url), playcount=playcount(a))
+            for a in first_n_ranks(charts['artist':], RANKS, playcount)
+        ]
+        self.title = u"Meist gespielte Bands vom %s" % self.when[:10]
+
+    def as_atom(self):
+        f = xmlbuilder.builder()
+        with f.feed(xmlns=ATOM_NS):
+            lastfm = 'http://www.last.fm/user/%s' % self.who
+            f.title(u"Meine last.fm-Hitparade")
+            with f.author:
+                f.name(self.who)
+            f.link(None, href=lastfm)
+            f.updated(self.when)
+            f.id('tag:drbeat.li,2010:lastfmcharts:%s' % self.who)
+            f.link(None, rel='self', href='http://%s%s' % (DOMAIN, PATH))
+            with f.self:
+                f.title(u"Meist gespielte Bands vom %s" % self.when[:10])
+                f.updated(self.when)
+                f.id('tag:%s,%s:%s:%s' % (DOMAIN, self.when[:10], PATH, self.who))
+                f.link(None, rel='alternate', type='text/html', href=lastfm + '/charts?charttype=weekly')
+                for term in self.tags:
+                    f.category(None, term=term)
+                with nested(f.content(type='xhtml'), f.div(xmlns=XHTML_NS), f.ol):
+                    for artist in self.artists:
+                        with f.li:
+                            f.a(artist['name'], href=artist['url'])
+                            f['(%s)' % artist['playcount']]
+        return str(f)
 
 def application(environ, start_response):
     """WSGI interface"""
@@ -79,7 +83,7 @@ def application(environ, start_response):
     ch = fetch_weekly_charts(user_id)
     if ch._name == 'weeklyartistchart' and prune_charts(ch):
         start_response('200 OK', [('Content-Type', 'application/atom+xml')])
-        return [make_feed(ch)]
+        return [Entry(ch).as_atom()]
     else:
         start_response('404 Not found', [('Content-Type', 'text/xml')])
         environ['rc'] = 1
